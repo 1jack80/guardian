d872bf3ab74bbec3fc8cd96afcf855346e15c74c
1jack80 <work.davidmainoo@gmail.com>
2023-09-12 14:36:49 +0000

    Remove mutex as i dont understand its use in full

--- memStore.go
+++ memStore.go
@@ -2,28 +2,22 @@ package guardian
 
 import (
 	"errors"
-	"sync"
 )
 
 // InMemoryStore is an in-memory implementation of the Storer interface.
 type InMemoryStore struct {
 	data map[string]*Session
-	lock sync.RWMutex
 }
 
 // NewInMemoryStore creates a new instance of InMemoryStore.
 func NewInMemoryStore() *InMemoryStore {
 	return &InMemoryStore{
 		data: make(map[string]*Session),
-		lock: sync.RWMutex{},
 	}
 }
 
 // get retrieves session data from the in-memory store.
 func (s *InMemoryStore) Get(sessionID string) (*Session, error) {
-	s.lock.RLock()
-	defer s.lock.RUnlock()
-
 	session, ok := s.data[sessionID]
 	if !ok {
 		return nil, errors.New("Session not found")
@@ -33,17 +27,12 @@ func (s *InMemoryStore) Get(sessionID string) (*Session, error) {
 
 // save saves a session into the in-memory store.
 func (s *InMemoryStore) Save(session *Session) error {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-
 	s.data[session.ID] = session
 	return nil
 }
 
 // delete deletes session data from the in-memory store.
 func (s *InMemoryStore) Delete(sessionID string) error {
-	s.lock.Lock()
-	defer s.lock.Unlock()
 
 	delete(s.data, sessionID)
 	return nil
